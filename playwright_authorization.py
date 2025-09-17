62574ccfdfa086ba5c8ea54f88f40341f528f057
katinagon <katinagon@yandex.ru>
2025-09-17 18:12:36 +0300

    Введение в Playwright. Практика с Playwright

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -17,5 +17,3 @@ with sync_playwright() as playwright:
     wrong_email_or_password_alert = page.get_by_test_id("login-page-wrong-email-or-password-alert")
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
-
-    page.wait_for_timeout(5000)
