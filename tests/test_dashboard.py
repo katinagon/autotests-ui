5303136aea8a3f46eee8e82b3072a1f76d6763a4
katinagon <katinagon@yandex.ru>
2025-10-29 16:32:03 +0300

    Знакомство с PageComponent

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -7,7 +7,7 @@ from pages.dashboard_page import DashboardPage
 @pytest.mark.regression
 def test_dashboard_displaying(dashboard_page_with_state: DashboardPage):
     dashboard_page_with_state.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
-    dashboard_page_with_state.navgobar.check_visible("username")
+    dashboard_page_with_state.navbar.check_visible("username")
     dashboard_page_with_state.check_visible_dashboard_title()
     dashboard_page_with_state.check_visible_scores_chart()
     dashboard_page_with_state.check_visible_courses_chart()
