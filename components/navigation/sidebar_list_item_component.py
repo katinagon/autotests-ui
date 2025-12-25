75cc3a6d77bf94b834c2053e7dbb7a50e34a7d0e
Kate <katinagon@yandex.ru>
2025-12-25 09:59:06 +0300

    Знакомство с PageFactory. Практика работы с Page Factory

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -1,27 +1,28 @@
 from typing import Pattern
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
+from elements.button import Button
+from elements.icon import Icon
+from elements.text import Text
 
 
 class SidebarListItemComponent(BaseComponent):
-    # Принимаем идентификатор компонента, например dashboard
     def __init__(self, page: Page, identifier: str):
         super().__init__(page)
 
-        # Формируем локаторы динамически
-        self.icon = page.get_by_test_id(f'{identifier}-drawer-list-item-icon')
-        self.title = page.get_by_test_id(f'{identifier}-drawer-list-item-title-text')
-        self.button = page.get_by_test_id(f'{identifier}-drawer-list-item-button')
+        self.icon = Icon(page, f'{identifier}-drawer-list-item-icon', 'Sidebar list icon')
+        self.title = Text(page, f'{identifier}-drawer-list-item-title-text', 'Sidebar list title')
+        self.button = Button(page, f'{identifier}-drawer-list-item-button', 'Sidebar list button')
 
     def check_visible(self, title: str):
-        expect(self.icon).to_be_visible()
+        self.icon.check_visible()
 
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text(title)
+        self.title.check_visible()
+        self.title.check_have_text(title)
 
-        expect(self.button).to_be_visible()
+        self.button.check_visible()
 
     def navigate(self, expected_url: Pattern[str]):
         self.button.click()
